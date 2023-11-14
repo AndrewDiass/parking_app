c17266444ccb63631dd2c79e9fa33c238ce07d9f
Andrew Dias <andrewd@wedoc.com.br>
2023-11-14 12:56:39 -0300

    wip: add theme and same widgets

---
+++ lib/src/shared/theme/pk_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final PK_THEME_DATA = ThemeData(
+  fontFamily: 'Roboto',
+  textTheme: TextTheme(
+    displayLarge: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+    displayMedium: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
+    displaySmall: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500),
+    labelLarge: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+    labelMedium: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+    labelSmall: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400),
+  ),
+);
